79447cbde657fce511c33434656700c4cf42525e
danricardo88 <daniel.ricardo88@outlook.com>
2023-04-18 19:42:44 -0300

    req-6 não vai ficar pra dps, req7 ok

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    new_list = search_news({"title": {"$regex": title, "$options": "-i"}})
+    tilte_results = []
+    for news in new_list:
+        tilte_results.append((news["title"], news["url"]))
+    return tilte_results
 
 
 # Requisito 8
